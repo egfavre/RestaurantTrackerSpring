6e844e7f9585ac06154c34ff67a58cbede45858a
Liz Favre <egfavre@gmail.com>
2016-06-23 10:45:55 -0400

    secure passwords

--- src/main/java/com/egfavre/RestaurantTrackerController.java
+++ src/main/java/com/egfavre/RestaurantTrackerController.java
@@ -22,9 +22,9 @@ public class RestaurantTrackerController {
     RestaurantRepository restaurants;
 
     @PostConstruct
-    public void init(){
+    public void init() throws PasswordStorage.CannotPerformOperationException {
         if (users.count() == 0){
-            User user = new User("Default", "default");
+            User user = new User("Default", PasswordStorage.createHash("pass"));
             users.save(user);
         }
     }
@@ -66,10 +66,10 @@ public class RestaurantTrackerController {
     public String login(String username, String password, HttpSession session) throws Exception {
         User user = users.findByName(username);
         if (user == null){
-            user = new User(username, password);
+            user = new User(username, PasswordStorage.createHash(password));
             users.save(user);
         }
-        else if (!user.password.equals(password)){
+        else if (PasswordStorage.verifyPassword(password, user.password)){
             throw new Exception("wrong password");
         }
         session.setAttribute("username", username);
